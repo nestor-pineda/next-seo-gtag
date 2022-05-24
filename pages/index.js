cce4bba89c3f42ebc448958c9a62a45fc4eec791
Nestor Pineda Elvira <npineda@tbscg.com>
2022-05-24 11:08:47 +0200

    changes in the handle click function

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,26 @@ import Link from "next/link";
 import * as gtag from "../lib/gtag";
 
 export default function Home() {
-  /*  const myclickHandler = () => {
+  const myclickHandler = () => {
     gtag.event({
-      action: "this_is_a_click",
-      category: "home_clicks",
-      label: "one link clicked",
-      value: "testing",
-    });
-  }; */
-
-  const search = () => {
-    ga.event({
-      action: "search",
-      params: {
-        search_term: query,
-      },
+      action: "submit_form",
+      category: "Contact",
+      label: "messageeee",
     });
   };
 
+  // const search = () => {
+  //   ga.event({
+  //     action: "search",
+  //     params: {
+  //       search_term: query,
+  //     },
+  //   });
+  // };
+
   return (
     <div>
-      <Link href="/about" onClick={() => search()}>
+      <Link href="/about" onClick={() => myclickHandler()}>
         <a>Test one</a>
       </Link>
     </div>
