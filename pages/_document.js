6e791d8a84c6503f5fb79728faf283d97b94c557
Nestor Pineda Elvira <npineda@tbscg.com>
2022-05-25 16:55:39 +0200

    gtag ok

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import * as gtag from "../lib/gtag";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
